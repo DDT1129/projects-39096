d097749f7f5aa84fa316277b65c8d0687dd6326a
DDT1129 <dai1129dt@gmail.com>
2023-05-18 22:45:35 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+  
+  def create
+    @comment = Comment.create(comment_params)  
+  
+    if @comment.save
+      redirect_to prototype_path(params[:prototype_id]), method: :get
+    else
+      
+      @prototype = Prototype.find(params[:prototype_id])
+      @comment = Comment.new
+      @comments = @prototype.comments.includes(:user)
+      render template: "prototypes/show"
+    end
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+
+end
